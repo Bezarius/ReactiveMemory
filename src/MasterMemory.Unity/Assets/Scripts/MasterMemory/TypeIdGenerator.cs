2c8b78b5e0ee00a7e162821d76bad63fdb90c8e3
m.pavlov <m.pavlov@corp.nekki.ru>
2023-07-16 21:13:57 +0300

    wip

---
+++ src/MasterMemory.Unity/Assets/Scripts/MasterMemory/TypeIdGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace ReactiveMemory
+{
+    internal static class TypeIdGenerator
+    {
+        private static int _index;
+
+        public static int Index => ++_index;
+    }
+}
---
+++ src/MasterMemory.Unity/Assets/Tests/MessagePackResolver.cs
@@ -0,0 +1,26 @@
+﻿using MessagePack;
+using MessagePack.Formatters;
+using MessagePack.Resolvers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReactiveMemory.Tests
+{
+    public class MessagePackResolver : IFormatterResolver
+    {
+        public static IFormatterResolver Instance = new MessagePackResolver();
+
+        MessagePackResolver()
+        {
+
+        }
+
+        public IMessagePackFormatter<T> GetFormatter<T>()
+        {
+            return ReactiveMemoryResolver.Instance.GetFormatter<T>()
+                ?? GeneratedResolver.Instance.GetFormatter<T>()
+                ?? StandardResolver.Instance.GetFormatter<T>();
+        }
+    }
+}
